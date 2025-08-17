a6550e35bdb45b4ae9014f01ecbce56e6849215d
VitoSB UESB <vitorsantiagobarreto123@gmail.com>
2025-08-17 19:19:42 -0300

    Listar e Detalhar Tópicos

--- src/main/java/com/vitor/forum_hub/controller/TopicoController.java
+++ src/main/java/com/vitor/forum_hub/controller/TopicoController.java
@@ -6,15 +6,16 @@ import com.vitor.forum_hub.domain.topico.Topico;
 import com.vitor.forum_hub.domain.topico.TopicoRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping("/topico")
+@RequestMapping("/topicos")
 //@SecurityRequirement(name = "bearer-key")
 public class TopicoController {
 
@@ -22,11 +23,26 @@ public class TopicoController {
     private TopicoRepository repository;
 
     @PostMapping
+    @Transactional
     public ResponseEntity cadastrar(@RequestBody DadosCadastroTopico dados, UriComponentsBuilder uriBuilder){
         var topico = new Topico(dados);
         repository.save(topico);
         var uri = uriBuilder.path("/topico/{id}").buildAndExpand(topico.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DadosDetalhamentoTopico(topico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosDetalhamentoTopico>> listar(@PageableDefault(size = 10) Pageable paginacao) {
+        var page = repository.findAll(paginacao).map(DadosDetalhamentoTopico::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var topico = repository.findById(id);
+
+        return ResponseEntity.ok(new DadosDetalhamentoTopico(topico.get()));
     }
 }
